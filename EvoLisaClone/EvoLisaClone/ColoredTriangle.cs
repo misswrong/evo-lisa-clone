ccff195f9674836fb7ddcf84231569854e711283
jaderd <jaderd@9a8b54a0-4f1a-11de-8ddf-d3afb9ac165e>
2009-06-07 21:00:14 +0000

    Rewriting the Application from scratch.

---
+++ EvoLisaClone/EvoLisaClone/ColoredTriangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace EvoLisaClone
+{
+    public class ColoredTriangle
+    {
+        private Point[] vertices;
+        
+        public Point[] Vertices 
+        { 
+            get
+            {
+                return this.vertices;
+            }
+            set
+            {
+                this.vertices = value;
+            }
+        }
+    }
+}
