4daf7918940db2652f37a306f36622fd94db563c
jaderd <jaderd@9a8b54a0-4f1a-11de-8ddf-d3afb9ac165e>
2009-06-13 17:56:28 +0000

    Implemented PointGenetics

---
+++ EvoLisaClone/EvoLisaClone/PointGenetics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace EvoLisaClone
+{
+    public sealed class PointGenetics
+    {
+        private static readonly PointGenetics instance = new PointGenetics();
+        private Random random = new Random();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static PointGenetics()
+        {
+        }
+
+        PointGenetics()
+        {
+        }
+
+        /// <summary>
+        /// The public Instance property to use
+        /// </summary>
+        public static PointGenetics Instance
+        {
+            get { return instance; }
+        }
+
+        public Point Create(int width, int height)
+        {
+            return new Point(random.Next(width), random.Next(height));
+        }
+    }
+}
