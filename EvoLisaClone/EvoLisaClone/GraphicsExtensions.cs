007fa53032fd7d27e6aac03bffcfb520ad7cfe8d
jaderd <jaderd@9a8b54a0-4f1a-11de-8ddf-d3afb9ac165e>
2009-06-13 04:37:21 +0000

    Created drawing and fitness methods/classes

---
+++ EvoLisaClone/EvoLisaClone/GraphicsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace EvoLisaClone
+{
+    public static class GraphicsExtensions
+    {
+        public static void FillColoredTriangle(Graphics graphics, ColoredTriangle coloredTriangle)
+        {
+            graphics.FillPolygon(coloredTriangle.Brush, coloredTriangle.Vertices.ToArray());
+        }
+
+        public static void RasterizeVectorDrawing(Graphics graphics, VectorDrawing vectorDrawing)
+        {
+            foreach (var coloredTriangle in vectorDrawing.Vectors)
+            {
+                FillColoredTriangle(graphics, coloredTriangle);
+            }
+        }
+    }
+}
---
+++ EvoLisaClone/EvoLisaCloneTest/GraphicsExtensionsTest.cs
@@ -0,0 +1,96 @@
+﻿using EvoLisaClone;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace EvoLisaCloneTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for GraphicsExtensionsTest and is intended
+    ///to contain all GraphicsExtensionsTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class GraphicsExtensionsTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        /// <summary>
+        ///A test for FillColoredTriangle
+        ///</summary>
+        [TestMethod()]
+        public void FillColoredTriangleTest1()
+        {
+            var expected = Color.Orange;
+            ColoredTriangle coloredTriangle = new ColoredTriangle()
+            {
+                Brush = new SolidBrush(expected),
+                Vertices = new Point[]
+                {
+                    new Point(-1,-1),
+                    new Point(-1,2),
+                    new Point(2,-1)
+                }
+            };
+            using (var bitmap = new Bitmap(1, 1))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    GraphicsExtensions.FillColoredTriangle(graphics, coloredTriangle);
+                }
+                var actual = bitmap.GetPixel(0, 0);
+                Assert.AreEqual(expected.R, actual.R);
+                Assert.AreEqual(expected.G, actual.G);
+                Assert.AreEqual(expected.B, actual.B);
+            }
+        }
+    }
+}
