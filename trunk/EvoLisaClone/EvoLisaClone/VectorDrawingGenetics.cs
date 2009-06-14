d8a3def7f13a552aeb63a73e060bbd84add3cc8c
jaderd <jaderd@9a8b54a0-4f1a-11de-8ddf-d3afb9ac165e>
2009-06-14 22:40:06 +0000

    Added VectorDrawingGenetics.Mutate

--- trunk/EvoLisaClone/EvoLisaClone/VectorDrawingGenetics.cs
+++ trunk/EvoLisaClone/EvoLisaClone/VectorDrawingGenetics.cs
@@ -54,15 +54,55 @@ namespace EvoLisaClone
             }
         }
 
-        public VectorDrawing Create(int width, int height)
+        public VectorDrawing Create(int width, int height, int length)
         {
+            var triangles = new List<ColoredTriangle>();
+            for (var i = 0; i < length; i++)
+            {
+                triangles.Add(ColoredTriangleGenetics.Instance.Create(width, height));
+            }
             return new VectorDrawing()
                 {
-                    Vectors = new ColoredTriangle[] 
-                    {
-                        ColoredTriangleGenetics.Instance.Create(width, height)
-                    }
+                    Vectors = triangles
                 };
+        }
+
+        public VectorDrawing CrossOver(VectorDrawing drawingA, VectorDrawing drawingB)
+        {
+            var triangles = new List<ColoredTriangle>();
+            for (var i = 0; i < drawingA.Vectors.Count(); i++)
+            {
+                if (i%2 == 0)
+                {
+                    triangles.Add(drawingA.Vectors.ElementAt(i));
+                }
+                else
+                {
+                    triangles.Add(drawingB.Vectors.ElementAt(i));
+                }
+            }
+            return new VectorDrawing()
+            {
+                 Vectors = triangles
+            };
+        }
+
+        public VectorDrawing Mutate(VectorDrawing drawing, int width, int height)
+        {
+            var count = drawing.Vectors.Count();
+            var triangles = drawing.Vectors.ToList();
+            if (count > 1 && random.Next(2) == 0)
+            {
+                triangles.RemoveAt(random.Next(count));
+            }
+            else
+            {
+                triangles.Add(ColoredTriangleGenetics.Instance.Create(width, height));
+            }
+            return new VectorDrawing()
+            {
+                Vectors = triangles
+            };
         }
     }
 }
