c36e54a81d840935f734103fc788839dbd47af4c
jaderd <jaderd@9a8b54a0-4f1a-11de-8ddf-d3afb9ac165e>
2009-06-15 02:59:47 +0000

    Added population > 1

--- trunk/EvoLisaClone/EvoLisaClone/BitmapCloner.cs
+++ trunk/EvoLisaClone/EvoLisaClone/BitmapCloner.cs
@@ -24,5 +24,50 @@ namespace EvoLisaClone
             }
             return parent;
         }
+
+        public VectorDrawing Clone(Bitmap bitmap, long distance, int populationSize)
+        {
+            var population = new Dictionary<VectorDrawing, long>();
+            while (population.Count < populationSize)
+            {
+                var newborn = VectorDrawingGenetics.Instance.Create(bitmap.Width, bitmap.Height, 1);
+                var newbornDistance = VectorDrawingGenetics.Instance.CalculateDistance(newborn, bitmap);
+                population.Add(newborn, newbornDistance);
+            }
+            var minDistance = 0L;
+            while (!population.Where(a => a.Value <= distance).Any())
+            {
+                if (populationSize == 1)
+                {
+                    var child = VectorDrawingGenetics.Instance.Mutate(population.First().Key, bitmap.Height, bitmap.Width);
+                    var childDistance = VectorDrawingGenetics.Instance.CalculateDistance(child, bitmap);
+                    population.Add(child, childDistance);
+                }
+                else
+                {
+                    for (var i = 1; i < populationSize; i++)
+                    {
+                        var child = VectorDrawingGenetics.Instance.CrossOver(population.ElementAt(i - 1).Key, population.ElementAt(i).Key);
+                        child = VectorDrawingGenetics.Instance.Mutate(child, bitmap.Width, bitmap.Height);
+                        var childDistance = VectorDrawingGenetics.Instance.CalculateDistance(child, bitmap);
+                        population.Add(child, childDistance);
+                    }
+                }
+                minDistance = population.Min(a => a.Value);
+                while (population.Count() > populationSize)
+                {
+                    var toBeRemoved = population.Where(a => a.Value != minDistance);
+                    if (toBeRemoved.Any())
+                    {
+                        population.Remove(toBeRemoved.First().Key);
+                    }
+                    else
+                    {
+                        population.Remove(population.First().Key);
+                    }
+                }
+            }
+            return population.Where(a => a.Value == minDistance).First().Key;
+        }
     }
 }
