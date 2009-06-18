478367002832b76eee2248a28f5eb0d85434ce53
jaderd <jaderd@9a8b54a0-4f1a-11de-8ddf-d3afb9ac165e>
2009-06-18 02:34:18 +0000

    Added Profiler

---
+++ EvoLisaClone/EvoLisaProfiler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EvoLisaClone;
+using System.IO;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace EvoLisaProfiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitmapCloner target = new BitmapCloner();
+            var fileName = "smile.png";
+            using (var bitmap = new Bitmap(fileName))
+            {
+                long expectedDistance = 1024 * 8;
+                var actual = target.Clone(bitmap, expectedDistance, 2);
+                var actualDistance = VectorDrawingGenetics.Instance.CalculateDistance(actual, bitmap);
+                using (var cloneBitmap = new Bitmap(bitmap.Width, bitmap.Height))
+                {
+                    using (var graphics = Graphics.FromImage(cloneBitmap))
+                    {
+                        GraphicsExtensions.RasterizeVectorDrawing(graphics, actual);
+                    }
+                    cloneBitmap.Save("smile.bmp", ImageFormat.Bmp);
+                }
+            }
+        }
+    }
+}
