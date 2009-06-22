7a42f146a9e168eae9b6a1a79585a4f0aa9c1daf
jaderd <jaderd@9a8b54a0-4f1a-11de-8ddf-d3afb9ac165e>
2009-06-22 23:43:21 +0000

    Removed and Sorted usings. Created GeneticFramework project.

--- EvoLisaClone/EvoLisaClone/GraphicsExtensions.cs
+++ EvoLisaClone/EvoLisaClone/GraphicsExtensions.cs
@@ -20,11 +20,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with EvoLisaClone.  If not, see <http://www.gnu.org/licenses/>.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Drawing;
+using System.Linq;
 
 namespace EvoLisaClone
 {
---
+++ EvoLisaClone/GeneticFramework/IGenetics.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="IGenetics.cs" company="Jader DIas">
+// Copyright (c) Jader Dias. All rights reserved.
+// </copyright>
+// <author>Jader Dias</author>
+// <email>jaderd@gmail.com</email>
+// <date>2009-06-21</date>
+
+// This file is part of GeneticFramework.
+//
+// GeneticFramework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GeneticFramework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with GeneticFramework.  If not, see <http://www.gnu.org/licenses/>.
+
+
+namespace GeneticFramework
+{
+    public interface IGenetics<T>
+    {
+        T CreateRandom();
+
+        T Mutate(T dna);
+
+        T Recombine(T dna1, T dna2);
+    }
+}
